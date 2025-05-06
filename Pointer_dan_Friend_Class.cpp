d552a6ae3ee674ec2e1b645d4dbe9927c2b75706
Faid arya <faid.arya.ft24@mail.umy.ac.id>
2025-05-06 18:21:34 +0700

    Membuat RumusPanjang Dan Lebar

--- Pointer_dan_Friend_Class.cpp
+++ Pointer_dan_Friend_Class.cpp
@@ -4,4 +4,19 @@ using namespace std;
 class PersegiPanjang
 {
     public: //akses modifer
+    int panjang, lebar;
+
+    public:
+    void input()
+    {
+        cout << "Masukkan Panjang : "; 
+        cin >> panjang; //Memasukkan Panjang
+        cout << "Masukkan Lebar : ";
+        cin >> lebar; //Memasukkan Lebar
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
 }
