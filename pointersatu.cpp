fd6a94c69a8306acfadf07704f87a108f12027f5
Faid arya <faid.arya.ft24@mail.umy.ac.id>
2025-05-06 18:49:51 +0700

    Membuat Pointer Reference refMhs

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -12,5 +12,11 @@ int main (){
     mahasiswa mhs{1}; //object mhs
     mhs.showNim(); //MemberAccesOperator
 
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Acces Operator
+    mhs.showNim();
+
+    
+
   
 }
